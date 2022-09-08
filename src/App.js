4f6b602ec68fac5f27f7db27c6683807daec8927
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-08 23:56:46 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Header from './components/Header/Header'
+import Home from './pages/Home/Home'
+import Destination from './pages/Destination/Destination'
+import Crew from './pages/Crew/Crew'
+import Technology from './pages/Technology/Technology'
 
 const App = () => {
   return (
-    <div>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
