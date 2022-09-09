3f110eabd69501f7e9d9ddb0e7f6ef49802c6ba3
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-10 00:09:26 +0200

    add global styles

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Home from './pages/Home/Home'
 import Destination from './pages/Destination/Destination'
 import Crew from './pages/Crew/Crew'
 import Technology from './pages/Technology/Technology'
+import { GlobalStyle } from './theme/GlobalStyles/GlobalStyles';
 
 const App = () => {
-  const location = window.location.pathname;
-  console.log(location);
   return (
     <div>
+      <GlobalStyle />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
