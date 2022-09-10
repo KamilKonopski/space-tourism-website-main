9de51129726941ee93a53e2465cd49c03df2c811
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-10 03:00:31 +0200

    add colors

--- src/utils/Theme/theme.js
+++ src/utils/Theme/theme.js
@@ -1,7 +1,7 @@
 export const theme = {
     colors: {
-        dark: '0B0D17',
-        light: 'D0D6F9',
-        white: 'FFFFFF',
+        dark: "#0B0D17",
+        light: "#D0D6F9",
+        white: "#FFFFFF",
     }
 }
