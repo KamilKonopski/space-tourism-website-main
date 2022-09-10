110d1139f9679e2d5a1dbb94927730b35af9a9cf
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-10 02:16:38 +0200

    change bg

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -1,8 +1,9 @@
-import homeBg from '../../assets/home/background-home-desktop.jpg';
+import mobileHomeBg from '../../assets/home/background-home-mobile.jpg';
 import styled from "styled-components";
 // import { theme } from '../../utils/Theme/theme';
 
 export const Wrapper = styled.div`
-    background: url(${homeBg});
+    background: url(${mobileHomeBg});
     font-family: 'Bellefair', serif;
+    height: 100vh;
 `;
