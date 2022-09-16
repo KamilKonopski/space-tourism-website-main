c7caceab1b594721a1525e392dae3572ced905e9
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-16 23:06:17 +0200

    add button to destination

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 import Header from '../../components/Header/Header';
 
 import { Button, ContentWrapper, ExploreButton, Heading, SubHeading, Text, Wrapper } from './Home.styled'
 const Home = () => {
+    let navigate = useNavigate();
+
+    const changePathToDestination = () => {
+        navigate("/destination");
+    };
+
     return (
         <Wrapper>
             <Header />
@@ -9,7 +17,7 @@ const Home = () => {
                 <SubHeading>so, you want to travel to</SubHeading>
                 <Heading>space</Heading>
                 <Text>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</Text>
-                <Button><ExploreButton to="/destination">explore</ExploreButton></Button>
+                <Button onClick={changePathToDestination}><ExploreButton>explore</ExploreButton></Button>
             </ContentWrapper>
         </Wrapper>
     );
