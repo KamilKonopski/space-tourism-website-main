ec09439d67750259bbb13ee0f61350b3aa48ce80
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-16 23:05:38 +0200

    add new routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Crew from './pages/Crew/Crew';
 import Destination from './pages/Destination/Destination';
 import Home from './pages/Home/Home';
 import Technology from './pages/Technology/Technology';
+import DestinationItem from './components/DestinationItem/DestinationItem';
+import CrewItem from './components/CrewItem/CrewItem';
+import TechnologyItem from './components/TechnologyItem/TechnologyItem';
 
 import { GlobalStyle } from './utils/GlobalStyles/GlobalStyles';
 import { theme } from './utils/Theme/theme';
@@ -20,8 +23,11 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/destination" element={<Destination />} />
+          <Route path="/destination/:name" element={<DestinationItem />} />
           <Route path="/crew" element={<Crew />} />
+          <Route path="/crew/:name" element={<CrewItem />} />
           <Route path="/technology" element={<Technology />} />
+          <Route path="/technology:name" element={<TechnologyItem />} />
          </Routes>
       </Router>
     </div>
