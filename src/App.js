acf4347036beed26ae593d09dfd337a7754460cb
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-10-01 00:26:12 +0200

    remove react fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Crew from "./pages/Crew/Crew";
@@ -8,16 +7,14 @@ import Technology from "./pages/Technology/Technology";
 
 function App() {
 	return (
-		<>
-			<Router>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/destination" element={<Destination />} />
-					<Route path="/crew" element={<Crew />} />
-					<Route path="/technology" element={<Technology />} />
-				</Routes>
-			</Router>
-		</>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/destination" element={<Destination />} />
+				<Route path="/crew" element={<Crew />} />
+				<Route path="/technology" element={<Technology />} />
+			</Routes>
+		</Router>
 	);
 }
 
