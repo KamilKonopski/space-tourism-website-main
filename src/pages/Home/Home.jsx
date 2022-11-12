559679c167ef94b595f8629dd0363960269d9e51
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-11-12 23:38:26 +0100

    add content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import Header from '../../components/Header/Header';
 function Home() {
     let navigate = useNavigate();
 
-    function changePathToDestination() {
+    function goToDestinationHandler() {
         navigate("/destination");
     };
 
@@ -15,8 +15,8 @@ function Home() {
             <div>
                 <h2>so, you want to travel to</h2>
                 <h1>space</h1>
-                <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
-                <button onClick={changePathToDestination}><span>explore</span></button>
+                <p>Let`s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we`ll give you a truly out of this world experience!</p>
+                <button onClick={goToDestinationHandler}><span>explore</span></button>
             </div>
         </>
     );
