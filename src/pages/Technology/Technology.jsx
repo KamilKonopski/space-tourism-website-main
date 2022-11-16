50ba93557c34eb8ff00b3818a918d6f298156c7a
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-11-16 22:46:50 +0100

    add image

--- src/pages/Technology/Technology.jsx
+++ src/pages/Technology/Technology.jsx
@@ -3,12 +3,16 @@ import { useState } from 'react';
 import Button from "../../components/Button/Button";
 import Header from "../../components/Header/Header";
 
+import vehicleImage from '../../assets/technology/image-launch-vehicle-landscape.jpg';
+import capsuleImage from '../../assets/technology/image-space-capsule-landscape.jpg';
+import spaceportImage from '../../assets/technology/image-spaceport-landscape.jpg';
+
 import { technologies } from "../../data/data";
 
 import classes from './Technology.module.css';
 
 function Technology() {
-    // const [image, setImage] = useState(douglasImage);
+    const [image, setImage] = useState(vehicleImage);
     const [name, setName] = useState(technologies[0].name);
     const [desc, setDesc] = useState(technologies[0].description);
 
@@ -16,14 +20,17 @@ function Technology() {
         const target = e.target.textContent
 
         if (target === '1') {
+            setImage(vehicleImage);
             setName(technologies[0].name)
             setDesc(technologies[0].description)
 
         } else if (target === '2') {
+            setImage(capsuleImage);
             setName(technologies[1].name)
             setDesc(technologies[1].description)
 
         } else if (target === '3') {
+            setImage(spaceportImage);
             setName(technologies[2].name)
             setDesc(technologies[2].description)
         }
@@ -37,6 +44,7 @@ function Technology() {
             <Button onChangeContent={changeContentHandler}>3</Button>
             <span>{name}</span>
             <span>{desc}</span>
+            <img src={image} alt={name} />
         </div>
     );
 };
