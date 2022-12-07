0d7241811af62a3f3618b3fdca5636225981a14b
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-12-07 21:04:03 +0100

    add classes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,12 +14,12 @@ function Home() {
     return (
         <div className={classes['home-page']}>
             <Header />
-            <div>
-                <h2>so, you want to travel to</h2>
-                <h1>space</h1>
-                <p>Let`s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we`ll give you a truly out of this world experience!</p>
-                <button onClick={goToDestinationHandler}><span>explore</span></button>
-            </div>
+            <main>
+                <h1 className={classes['home-page__heading']}>so, you want to travel to</h1>
+                <h2 className={classes['home-page__space']}>space</h2>
+                <p className={classes['home-page__description']}>Let`s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we`ll give you a truly out of this world experience!</p>
+                <button className={classes['home-page__btn']} onClick={goToDestinationHandler}><span className={classes['home-page__btn-explore']}>explore</span></button>
+            </main>
         </div>
     );
 };
