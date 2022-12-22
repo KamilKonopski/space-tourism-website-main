65aca06c6ec093a3cde06895c392956c2abf4831
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-12-22 23:05:01 +0100

    add content class

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,9 +15,11 @@ function Home() {
         <div className={classes['home-page']}>
             <Header />
             <main className={classes['home-page__content-wrapper']}>
-                <h1 className={classes['home-page__heading']}>so, you want to travel to</h1>
-                <h2 className={classes['home-page__space']}>space</h2>
-                <p className={classes['home-page__description']}>Let`s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we`ll give you a truly out of this world experience!</p>
+                <div className={classes['home-page__content']}>
+                    <h1 className={classes['home-page__heading']}>so, you want to travel to</h1>
+                    <h2 className={classes['home-page__space']}>space</h2>
+                    <p className={classes['home-page__description']}>Let`s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we`ll give you a truly out of this world experience!</p>
+                </div>
                 <button className={classes['home-page__btn']} onClick={goToDestinationHandler}><span className={classes['home-page__btn-explore']}>explore</span></button>
             </main>
         </div>
